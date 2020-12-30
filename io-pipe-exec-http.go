61daa60cca5bd1ba55ffd59f6ac8972b24cc12bc
Ping Zhang <pingzh@umich.edu>
2020-12-29 21:53:00 -0800

    Create io-pipe-exec-http.go

---
+++ io-pipe-exec-http.go
@@ -0,0 +1,25 @@
+
+
+package main
+
+import (
+	"io"
+	"net/http"
+	"os/exec"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	cmd := exec.Command("bash", "-c", "ls -alh; lsl")
+	pipeReader, pipeWriter := io.Pipe()
+	defer pipeWriter.Close()
+
+	cmd.Stdout = pipeWriter
+	cmd.Stderr = pipeWriter
+	go io.Copy(w, pipeReader)
+	cmd.Run()
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
